450491ace1fb8aa9d031a8d4fcda0ec42c3f63b5
Kirill Khudyakov <hudiakov@gmail.com>
2020-08-03 13:02:17 +0300

    Скорректировал задачу
    Урок 3

--- Lesson3/Lesson3/Solutions.m
+++ Lesson3/Lesson3/Solutions.m
@@ -12,7 +12,7 @@
 @implementation Solutions
 
 +(void)executeTask1 {
-    printf("\nTask1\nСоздать массив строк и вывести его в консоль используя цикл.\n");
+    printf("\nTask1\nСоздать программу, которая будет выводить список строк как минимум тремя способами: в цикле while, в цикле for-in с дополнительным условием, с применением break и continue.\n");
     [[Strings new] run];
 }
 +(void)executeTask2 {}
